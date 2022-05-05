d65c2022629c860dfe2bf75cccd17a66b19a126d
Darren Coutts <couttsdarren@outlook.com>
2022-05-05 02:55:06 +0100

    :sparkles: Configure Package

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * :package_name configuration
+ * formulate configuration
  */
 return [
 
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace AppKit\:package_name_php\Tests;
+namespace AppKit\Formulate\Tests;
 
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
-use AppKit\:package_name_php\:package_name_phpServiceProvider;
-use AppKit\:package_name_php\Facades\:package_name_php;
+use AppKit\Formulate\FormulateServiceProvider;
+use AppKit\Formulate\Facades\Formulate;
 
 class TestCase extends OrchestraTestCase
 {
@@ -33,7 +33,7 @@ class TestCase extends OrchestraTestCase
      */
     protected function getPackageProviders($app)
     {
-        return [:package_name_phpServiceProvider::class];
+        return [FormulateServiceProvider::class];
     }
 
     /**
@@ -45,7 +45,7 @@ class TestCase extends OrchestraTestCase
     protected function getPackageAliases($app)
     {
         return [
-            ':package_name_php' => :package_name_php::class
+            'Formulate' => Formulate::class
         ];
     }
 
