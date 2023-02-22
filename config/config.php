9345b90913e534176d16363edb6f1942893f3680
Darren Coutts <couttsdarren@outlook.com>
2023-02-22 19:58:25 +0000

    add dark mode

--- config/config.php
+++ config/config.php
@@ -12,7 +12,7 @@ return [
 
     'classes' => [
         'field_error' => 'mt-2 text-red-800 border-red-800',
-        'field' => 'rounded-md shadow-sm border-gray-300 focus:border-teal-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 block mt-1 w-full',
+        'field' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm',
         'form_error' => 'mb-4 font-bold text-white bg-red-800 p-3 rounded-md',
         'group' => 'mb-4',
         'label' => 'block font-medium text-sm text-gray-700',
