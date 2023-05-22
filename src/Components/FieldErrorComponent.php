46a274f9317a87b4836684901577c7d1175b4b66
darrencoutts118 <darrencoutts118@users.noreply.github.com>
2023-05-22 15:20:47 +0000

    PSR-12 Coding Standards

--- src/Components/FieldErrorComponent.php
+++ src/Components/FieldErrorComponent.php
@@ -30,7 +30,7 @@ class FieldErrorComponent extends Component
     public function shouldRender()
     {
         // get the errors that are being shared with the View
-        $errors = View::shared('errors', new ViewErrorBag);
+        $errors = View::shared('errors', new ViewErrorBag());
 
         // check that if we have errors in this bag or not, which will determine if the component is rendered or not
         return $errors->has($this->field->name);
